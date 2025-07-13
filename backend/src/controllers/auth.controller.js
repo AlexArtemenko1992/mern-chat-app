92fc08d0a1111a78822f351e9b1d40d7cf317cb9
Александр Артеменко <aleksandrartemenko@MacBook-Pro-Aleksandr.local>
2025-07-13 15:56:00 +0300

    MERN-initial structure back/front

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req, res) => {
+  res.send("Login Page");
+};
+
+export const register = (req, res) => {
+  res.send("Register Page");
+};
+
+export const logout = (req, res) => {
+  res.send("Logout Page");
+};
